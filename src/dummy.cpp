6589000102718b5d84d382e6886932894e3bf1c2
kento-takeuchi@cybozu.co.jp <lucky.rabbitfoot141@gmail.com>
2023-07-04 09:40:50 +0000

    change header file path

--- src/dummy.cpp
+++ src/dummy.cpp
@@ -1,4 +1,4 @@
-#include "dummy.h"
+#include "../include/dummy.h"
 
 int one() {
 	return 1;
--- test/test_dummy.cpp
+++ test/test_dummy.cpp
@@ -1,6 +1,6 @@
 #include "gtest/gtest.h"
 
-#include "dummy.h"
+#include "../include/dummy.h"
 
 TEST(test_dummy, check_return_zero) {
 	EXPECT_EQ(one(), 1);
