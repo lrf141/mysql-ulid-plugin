e91f366da218fa2e7cc7d43e83dd9eb61efd6cb6
kento-takeuchi <lucky.rabbitfoot141@gmail.com>
2023-07-05 00:21:13 +0900

    set random number part as uint8_t

--- src/ulid_generator.h
+++ src/ulid_generator.h
@@ -1,12 +1,22 @@
 #ifndef MYSQL_ULID_ULID_GENERATOR_H
 #define MYSQL_ULID_ULID_GENERATOR_H
+
 #include <iostream>
 #include <ctime>
 #include <mysql_time.h>
+#include <random>
+
+// 6 byte
+#define TIMESTAMP_PART_SIZE 6
+// 10 byte
+#define RANDOM_NUMBER_PART_SIZE 10
+
+static std::uniform_int_distribution<uint8_t> Rand255(0, 255);
 
 struct ULID {
     // ULID structured by 16 octet data
     uint8_t data[16];
+
     ULID() {
         for (unsigned char & i : data) {
             i = 0;
@@ -20,6 +30,19 @@ struct ULID {
         ulid.data[3] = static_cast<uint8_t>(timestamp >> 16);
         ulid.data[4] = static_cast<uint8_t>(timestamp >> 8);
         ulid.data[5] = static_cast<uint8_t>(timestamp);
+    }
+
+    inline void GenerateRandomNumberPart(std::mt19937& mt, ULID& ulid) {
+        ulid.data[6] = Rand255(mt);
+        ulid.data[7] = Rand255(mt);
+        ulid.data[8] = Rand255(mt);
+        ulid.data[9] = Rand255(mt);
+        ulid.data[10] = Rand255(mt);
+        ulid.data[11] = Rand255(mt);
+        ulid.data[12] = Rand255(mt);
+        ulid.data[13] = Rand255(mt);
+        ulid.data[14] = Rand255(mt);
+        ulid.data[15] = Rand255(mt);
     }
 };
 
--- test/test_ulid_generator.cpp
+++ test/test_ulid_generator.cpp
@@ -7,7 +7,26 @@ TEST(test_generate_timestamp_part, set_as_binary) {
     // first 42 bits are all one.
     time_t timestamp = 281474976710655L;
     ulid.GenerateTimestampPart(timestamp, ulid);
-    for (int i = 0; i < 6; i++) {
+    int i;
+    for (i = 0; i < TIMESTAMP_PART_SIZE; i++) {
         ASSERT_EQ(ulid.data[i], static_cast<uint8_t>(255));
     }
+    for (; i < RANDOM_NUMBER_PART_SIZE; i++) {
+        ASSERT_EQ(ulid.data[i], static_cast<uint8_t>(0));
+    }
 };
+
+TEST(test_generate_random_number_part, set_as_binary) {
+    ULID ulid;
+    std::random_device rnd;
+    std::mt19937 mt(rnd());
+    ulid.GenerateRandomNumberPart(mt, ulid);
+    int i;
+    for (i = 0; i < TIMESTAMP_PART_SIZE; i++) {
+        ASSERT_EQ(ulid.data[i], static_cast<uint8_t>(0));
+    }
+    for (; i < RANDOM_NUMBER_PART_SIZE; i++) {
+        ASSERT_GE(ulid.data[i], static_cast<uint8_t>(0));
+        ASSERT_LE(ulid.data[i], static_cast<uint8_t>(255));
+    }
+}
