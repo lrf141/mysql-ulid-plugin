667bd8cbe7a7d8c59e3780c42ad63de1c23a2e9f
kento-takeuchi <lucky.rabbitfoot141@gmail.com>
2023-07-05 00:48:59 +0900

    change udf init condition

--- src/ulid.cpp
+++ src/ulid.cpp
@@ -1,11 +1,19 @@
+#include <limits>
 #include <mysql.h>
+#include <cstring>
 
-extern "C" bool ulid_init(UDF_INIT *, UDF_ARGS *, char *) {
-  return false;
+extern "C" bool ulid_init(UDF_INIT *, UDF_ARGS *, char *message) {
+#ifndef __SIZEOF_INT128__
+    return false;
+#else
+    // No support if it does not use uint128_t
+    strcpy(message, "if not support __uint128_t, can not use ULID().");
+    return true;
+#endif
 }
 
 extern "C" void ulid_deinit(UDF_INIT *){}
 
 extern "C" char *ulid(UDF_INIT *, UDF_ARGS *, char *result, unsigned long *length, char *is_null, char *error) {
-  return nullptr;
+    return nullptr;
 }
