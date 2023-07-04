fa27869e0e272d4c49b869f7ec8259c6900fb207
kento-takeuchi <lucky.rabbitfoot141@gmail.com>
2023-07-05 01:07:06 +0900

    remove init condition

--- src/ulid.cpp
+++ src/ulid.cpp
@@ -1,14 +1,7 @@
 #include <mysql.h>
-#include <cstring>
 
-extern "C" bool ulid_init(UDF_INIT *, UDF_ARGS *, char *message) {
-#ifndef __SIZEOF_INT128__
+extern "C" bool ulid_init(UDF_INIT *, UDF_ARGS *, char *) {
     return false;
-#else
-    // No support if it does not use uint128_t
-    strcpy(message, "if not support __uint128_t, can not use ULID().");
-    return true;
-#endif
 }
 
 extern "C" void ulid_deinit(UDF_INIT *){}
