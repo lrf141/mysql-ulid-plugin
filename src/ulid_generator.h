9c3cdb7ade960f64a86c7bb7fc946be3e7f230d1
kento-takeuchi <lucky.rabbitfoot141@gmail.com>
2023-07-09 18:30:19 +0900

    add crockford's base32

--- src/ulid_generator.h
+++ src/ulid_generator.h
@@ -10,6 +10,8 @@
 #define TIMESTAMP_PART_SIZE 6
 // 10 byte
 #define RANDOM_NUMBER_PART_SIZE 10
+// Encoded characters size + '\0'
+#define ULID_STRING_LENGTH 27
 
 static std::uniform_int_distribution<uint8_t> Rand255(0, 255);
 static const char EncodeCharacters[33] = "0123456789ABCDEFGHJKMNPQRSTVWXYZ";
@@ -24,23 +26,71 @@ struct ULID {
         }
     }
 
-    inline void GenerateTimestampPart(time_t timestamp, ULID& ulid) {
-        ulid.data[0] = static_cast<uint8_t>(timestamp >> 40);
-        ulid.data[1] = static_cast<uint8_t>(timestamp >> 32);
-        ulid.data[2] = static_cast<uint8_t>(timestamp >> 24);
-        ulid.data[3] = static_cast<uint8_t>(timestamp >> 16);
-        ulid.data[4] = static_cast<uint8_t>(timestamp >> 8);
-        ulid.data[5] = static_cast<uint8_t>(timestamp);
-    }
-
-    inline void GenerateRandomnessPart(std::mt19937& mt, ULID& ulid) {
-        for (int i = TIMESTAMP_PART_SIZE; i < RANDOM_NUMBER_PART_SIZE; i++) {
-            addNumberAsUint8t(Rand255(mt), i, ulid);
+    inline void GenerateTimestampPart(time_t timestamp) {
+        for (int i = 0, bitShift = 40; i < TIMESTAMP_PART_SIZE; i++, bitShift -= 8) {
+            addDataByIndexAsUint8t(static_cast<uint8_t>(timestamp >> bitShift), i);
         }
     }
 
-    inline void addNumberAsUint8t(uint8_t n, int index, ULID& ulid) {
-        ulid.data[index] = n;
+    inline void GenerateRandomnessPart(std::mt19937& mt) {
+        for (int i = TIMESTAMP_PART_SIZE; i < RANDOM_NUMBER_PART_SIZE; i++) {
+            addDataByIndexAsUint8t(Rand255(mt), i);
+        }
+    }
+
+    inline void addDataByIndexAsUint8t(uint8_t n, int index) {
+        data[index] = n;
+    }
+
+    /**
+     * Encode as Crockford's Base32
+     * See: https://www.crockford.com/base32.html
+     * @param encodedData
+     * @return
+     */
+    inline std::string Encode() {
+        char encodedData[ULID_STRING_LENGTH];
+        encodedData[ULID_STRING_LENGTH - 1] = '\0';
+
+        // Timestamp Part Encode
+        encodedData[0] = EncodeCharacters[(data[0] & 224) >> 5];
+        encodedData[1] = EncodeCharacters[(data[0] & 31)];
+        encodedData[2] = EncodeCharacters[(data[1] & 248) >> 3];
+        encodedData[3] = EncodeCharacters[((data[1] & 7) << 2) | ((data[2] & 192) >> 6)];
+        encodedData[4] = EncodeCharacters[(data[2] & 62) >> 1];
+        encodedData[5] = EncodeCharacters[((data[2] & 1) << 4) | ((data[3] & 240) >> 4)];
+        encodedData[6] = EncodeCharacters[((data[3] & 15) << 1) | ((data[4] & 128) >> 7)];
+        encodedData[7] = EncodeCharacters[(data[4] & 124) >> 2];
+        encodedData[8] = EncodeCharacters[((data[4] & 3) << 3) | (data[5] & 224) >> 5];
+        encodedData[9] = EncodeCharacters[data[5] & 31];
+
+        // Randomness Part Encode
+        encodedData[10] = EncodeCharacters[(data[6] & 248) >> 3];
+        encodedData[11] = EncodeCharacters[((data[6] & 7) << 2) | ((data[7] & 192) >> 6)];
+        encodedData[12] = EncodeCharacters[(data[7] & 62) >> 1];
+        encodedData[13] = EncodeCharacters[((data[7] & 1) << 4) | ((data[8] & 240) >> 4)];
+        encodedData[14] = EncodeCharacters[((data[8] & 15) << 1) | ((data[9] & 128) >> 7)];
+        encodedData[15] = EncodeCharacters[(data[9] & 124) >> 2];
+        encodedData[16] = EncodeCharacters[((data[9] & 3) << 3) | ((data[10] & 224) >> 5)];
+        encodedData[17] = EncodeCharacters[data[10] & 31];
+        encodedData[18] = EncodeCharacters[(data[11] & 248) >> 3];
+        encodedData[19] = EncodeCharacters[((data[11] & 7) << 2) | ((data[12] & 192) >> 6)];
+        encodedData[20] = EncodeCharacters[(data[12] & 62) >> 1];
+        encodedData[21] = EncodeCharacters[((data[12] & 1) << 4) | ((data[13] & 240) >> 4)];
+        encodedData[22] = EncodeCharacters[((data[13] & 15) << 1) | ((data[14] & 128) >> 7)];
+        encodedData[23] = EncodeCharacters[(data[14] & 124) >> 2];
+        encodedData[24] = EncodeCharacters[((data[14] & 3) << 3) | ((data[15] & 224) >> 5)];
+        encodedData[25] = EncodeCharacters[data[15] & 31];
+
+        return std::string(encodedData);
+    }
+
+    inline std::string Create(time_t timestamp) {
+        std::random_device randomDevice;
+        std::mt19937 mt(randomDevice());
+        GenerateTimestampPart(timestamp);
+        GenerateRandomnessPart(mt);
+        return Encode();
     }
 };
 
--- test/test_ulid_generator.cpp
+++ test/test_ulid_generator.cpp
@@ -1,4 +1,5 @@
 #include "gtest/gtest.h"
+#include <chrono>
 
 #include "ulid_generator.h"
 
@@ -6,7 +7,7 @@ TEST(test_generate_timestamp_part, set_as_binary) {
     ULID ulid;
     // first 42 bits are all one.
     time_t timestamp = 281474976710655L;
-    ulid.GenerateTimestampPart(timestamp, ulid);
+    ulid.GenerateTimestampPart(timestamp);
     int i;
     for (i = 0; i < TIMESTAMP_PART_SIZE; i++) {
         ASSERT_EQ(ulid.data[i], static_cast<uint8_t>(255));
@@ -20,7 +21,7 @@ TEST(test_generate_random_number_part, set_as_binary) {
     ULID ulid;
     std::random_device rnd;
     std::mt19937 mt(rnd());
-    ulid.GenerateRandomnessPart(mt, ulid);
+    ulid.GenerateRandomnessPart(mt);
     int i;
     for (i = 0; i < TIMESTAMP_PART_SIZE; i++) {
         ASSERT_EQ(ulid.data[i], static_cast<uint8_t>(0));
@@ -34,9 +35,9 @@ TEST(test_generate_random_number_part, set_as_binary) {
 TEST(test_add_number_as_uint8t, set_as_uint8t) {
     ULID ulid;
     time_t timestamp = 281474976710655L;
-    ulid.GenerateTimestampPart(timestamp, ulid);
+    ulid.GenerateTimestampPart(timestamp);
     for(int i = TIMESTAMP_PART_SIZE; i < RANDOM_NUMBER_PART_SIZE; i++) {
-        ulid.addNumberAsUint8t(static_cast<uint8_t>(255), i, ulid);
+        ulid.addDataByIndexAsUint8t(static_cast<uint8_t>(255), i);
     }
     int i;
     for (i = 0; i < TIMESTAMP_PART_SIZE; i++) {
@@ -45,4 +46,32 @@ TEST(test_add_number_as_uint8t, set_as_uint8t) {
     for (; i < RANDOM_NUMBER_PART_SIZE; i++) {
         ASSERT_EQ(ulid.data[i], static_cast<uint8_t>(255));
     }
+}
+
+TEST(test_encode, encode_as_crockfords_base32) {
+    ULID ulid;
+    time_t time = 1688894191930L;
+    ulid.GenerateTimestampPart(time);
+    for(int i = TIMESTAMP_PART_SIZE; i < RANDOM_NUMBER_PART_SIZE; i++) {
+        ulid.addDataByIndexAsUint8t(static_cast<uint8_t>(255), i);
+    }
+    std::string ulidString = ulid.Encode();
+    ASSERT_EQ(ulidString, "01H4WZ0E9TZZZZZZR000000000");
+    ASSERT_EQ(ulidString.length(), ULID_STRING_LENGTH - 1);
+}
+
+TEST(test_encode, correspond_timestamp_and_lex_order) {
+    ULID ulid;
+    time_t time = 1688894191930L;
+    ulid.GenerateTimestampPart(time);
+    for(int i = TIMESTAMP_PART_SIZE; i < RANDOM_NUMBER_PART_SIZE; i++) {
+        ulid.addDataByIndexAsUint8t(static_cast<uint8_t>(255), i);
+    }
+    std::string ulidString1 = ulid.Encode();
+
+    ulid.GenerateTimestampPart(time + 1);
+    std::string ulidString2 = ulid.Encode();
+
+    int diffCount = ulidString1.compare(ulidString2);
+    ASSERT_LT(diffCount, 0);
 }
