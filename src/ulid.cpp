25f7bc8e4903be829de7adc0896ee79fda5d3088
kento-takeuchi <lucky.rabbitfoot141@gmail.com>
2023-07-09 23:22:22 +0900

    add udf main process

--- src/ulid.cpp
+++ src/ulid.cpp
@@ -1,4 +1,8 @@
+#include <ctime>
 #include <mysql.h>
+#include <mysql/udf_registration_types.h>
+#include <cstring>
+#include "ulid_generator.h"
 
 extern "C" bool ulid_init(UDF_INIT *, UDF_ARGS *, char *) {
     return false;
@@ -6,6 +10,18 @@ extern "C" bool ulid_init(UDF_INIT *, UDF_ARGS *, char *) {
 
 extern "C" void ulid_deinit(UDF_INIT *){}
 
-extern "C" char *ulid(UDF_INIT *, UDF_ARGS *, char *result, unsigned long *length, char *is_null, char *error) {
-    return nullptr;
+extern "C" char *ulid(UDF_INIT *, UDF_ARGS *, char *result, unsigned long *length, char *is_null, char *) {
+    std::cout << "enter ulid function" << std::endl;
+    time_t now;
+    ULID ulidGenerator;
+
+    time(&now);
+    std::string ulidString = ulidGenerator.Create(now);
+    std::cout << ulidString << std::endl;
+    strcpy(result, ulidString.c_str());
+    *length = strlen(result);
+    std::cout << *result << std::endl;
+    // TODO: return human readable string.
+    // NOW-ex: 0x303030314A414E474A4E4D434D46303752303030303030303030
+    return result;
 }
