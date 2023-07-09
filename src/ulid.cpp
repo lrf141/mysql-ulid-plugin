8af189e615394619a870eb95f1bec1901cf4a1cc
kento-takeuchi <lucky.rabbitfoot141@gmail.com>
2023-07-09 23:26:54 +0900

    remove debug print and add comment

--- src/ulid.cpp
+++ src/ulid.cpp
@@ -11,16 +11,14 @@ extern "C" bool ulid_init(UDF_INIT *, UDF_ARGS *, char *) {
 extern "C" void ulid_deinit(UDF_INIT *){}
 
 extern "C" char *ulid(UDF_INIT *, UDF_ARGS *, char *result, unsigned long *length, char *is_null, char *) {
-    std::cout << "enter ulid function" << std::endl;
     time_t now;
     ULID ulidGenerator;
 
     time(&now);
+    // TODO: check randomness pattern
     std::string ulidString = ulidGenerator.Create(now);
-    std::cout << ulidString << std::endl;
     strcpy(result, ulidString.c_str());
     *length = strlen(result);
-    std::cout << *result << std::endl;
     // TODO: return human readable string.
     // NOW-ex: 0x303030314A414E474A4E4D434D46303752303030303030303030
     return result;
